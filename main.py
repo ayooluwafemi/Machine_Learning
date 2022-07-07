d53ff55e8f793ca66dc38fb893831e99f18c087b
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 23:23:18 +0100

    Percentiles

--- main.py
+++ main.py
@@ -30,6 +30,17 @@ speed = [32,111,138,28,59,77,97]
 x = numpy.var(speed)
 print(x)
 
+# Percentiles:What is the 75. percentile? 
+# The answer is 43, meaning that 75% of the people are 43 or younger
+ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
+x = numpy.percentile(ages, 75)
+print(x)
+
+# What is the age that 90% of the people are younger than?
+ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
+x = numpy.percentile(ages, 90)
+print(x)
+
 
 
 
