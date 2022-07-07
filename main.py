4811cf5d1db6543e2fa027ef1c4fde18f5557de7
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 22:54:12 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Machine Learning is making the computer learn from studying data and statistics.
+# Machine Learning is a step into the direction of artificial intelligence (AI).
+# Machine Learning is a program that analyses data and learns to predict the outcome.
+import numpy
+
+# Mean value is the average value
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+x = numpy.mean(speed)
+print(x)
