8ea4420045dd8908a2e9904feba1e2e959471889
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 23:29:15 +0100

    Data Distribution

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Machine Learning is a program that analyses data and learns to predict the outcome.
 import numpy
 from scipy import stats
+import matplotlib.pyplot as plt
 
 # Mean value is the average value
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
@@ -30,7 +31,7 @@ speed = [32,111,138,28,59,77,97]
 x = numpy.var(speed)
 print(x)
 
-# Percentiles:What is the 75. percentile? 
+# Percentiles: What is the 75. percentile? 
 # The answer is 43, meaning that 75% of the people are 43 or younger
 ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
 x = numpy.percentile(ages, 75)
@@ -41,7 +42,23 @@ ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
 x = numpy.percentile(ages, 90)
 print(x)
 
+# Data Distribution: Create an array containing 250 random floats between 0 and 5:
+x = numpy.random.uniform(0.0, 5.0, 250)
+print(x)
 
+# We use the array from the example above to draw a histogram with 5 bars
+# 52 values are between 0 and 1
+# 48 values are between 1 and 2
+# 49 values are between 2 and 3
+# 51 values are between 3 and 4
+# 50 values are between 4 and 5
+x = numpy.random.uniform(0.0, 5.0, 250)
+plt.hist(x, 5)
+plt.show()
+
+x = numpy.random.uniform(0.0, 5.0, 100000)
+plt.hist(x, 100)
+plt.show()
 
 
 
