44f5562d88f072482239653ad1faabf1e3341bb0
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 23:33:49 +0100

    Scatter Plot

--- main.py
+++ main.py
@@ -60,6 +60,20 @@ x = numpy.random.uniform(0.0, 5.0, 100000)
 plt.hist(x, 100)
 plt.show()
 
+# We use the array from the numpy.random.normal() method, with 100000 values,  to draw a histogram with 100 bars.
+# We specify that the mean value is 5.0, and the standard deviation is 1.0.
+# Meaning that the values should be concentrated around 5.0, and rarely further away than 1.0 from the mean.
+# And as you can see from the histogram, most values are between 4.0 and 6.0, with a top at approximately 5.0.
+x = numpy.random.normal(5.0, 1.0, 100000)
+plt.hist(x, 100)
+plt.show()
+
+# Machine Learning - Scatter Plot
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+plt.scatter(x, y)
+plt.show()
+
 
 
 
