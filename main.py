f7dfd23b6c2214e1ac354c2c04856832c2215b52
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-08 18:51:05 +0100

    Completed

--- main.py
+++ main.py
@@ -111,21 +111,3 @@ y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
 mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
 speed = mymodel(17)
 print(speed)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
